4122f9f00e702cbc88efa14fea8df201a7b4292d
karnelll <165611407+karnelll@users.noreply.github.com>
2024-11-29 03:46:09 +0900

    [fix] build error

--- fe/src/app/event-maps/[id]/[nonMemberId]/load-mappin-edit/page.tsx
+++ fe/src/app/event-maps/[id]/[nonMemberId]/load-mappin-edit/page.tsx
@@ -98,7 +98,7 @@ export default function LinkEditPage() {
   };
 
   const handleBack = () => {
-    setShowExitModal(true); 
+    setShowExitModal(true);
   };
 
   const handleExitConfirm = () => {
