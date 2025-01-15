defa01bd8fae2007a817e98dd2923d953e0f0719
SEOHEE CHOI <165611407+karnelll@users.noreply.github.com>
2025-01-15 17:28:11 +0900

    [refactor] apply review feedback (#116)
    * [feature] Add MSW setup for API mocking
    
    * [chore] apply review feedback

--- fe/src/app/mocks/handlers.ts
+++ fe/src/app/mocks/handlers.ts
@@ -1,3 +1,10 @@
 import { testGetHandler, testPostHandler, usersHandler } from "./handler/test";
 
+/**
+ * 핸들러 목록
+ * - `testGetHandler`: 간단한 GET 요청 핸들러
+ * - `testPostHandler`: 간단한 POST 요청 핸들러
+ * - `usersHandler`: 사용자 정보를 반환하는 핸들러
+ */
+
 export const handlers = [testGetHandler, testPostHandler, usersHandler];
--- fe/src/app/mocks/server.ts
+++ fe/src/app/mocks/server.ts
@@ -1,4 +1,5 @@
 import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
 
+// 서버 초기화
 export const server = setupServer(...handlers);
