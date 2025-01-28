287c6c6beee2506f679b0b76d764a2a6a67cf6e2
choihooo <hochoi8621@naver.com>
2025-01-28 18:57:34 +0900

    [feature] 논멤버 예시 데이터 생성

--- fe/src/app/mocks/data/nonMember.ts
+++ fe/src/app/mocks/data/nonMember.ts
@@ -1,11 +1,11 @@
 let nonMember = [
-    {
-        "created_at" : "2023-01-28T15:45:30.500Z",
-        "updated_at" : "2023-02-28T15:45:30.500Z",
-        "name" : "최호",
-        "password": "1234",
-        "share_url_id": "1",
-        "profile_svg": "1.svg",
-        "profile_lock_svg": "1.svg"
-    }
-]
+  {
+    created_at: "2023-01-28T15:45:30.500Z",
+    updated_at: "2023-02-28T15:45:30.500Z",
+    name: "최호",
+    password: "1234",
+    share_url_id: "1",
+    profile_svg: "1.svg",
+    profile_lock_svg: "1.svg",
+  },
+];
---
+++ fe/src/app/mocks/data/shareUrl.ts
@@ -0,0 +1,14 @@
+let shareUrl = [
+  {
+    created_at: "2023-01-28T15:45:30.500Z",
+    updated_at: "2023-02-28T15:45:30.500Z",
+    neighborhood: "홍익대학교",
+    px: 126.9256698,
+    py: 37.5507353,
+    eventName: "힙한모임",
+    url: "https://moping.co.kr/share/%ED%9E%99%ED%95%9C%EB%AA%A8%EC%9E%84/%ED%99%8D%EC%9D%B5%EB%8C%80%ED%95%99%EA%B5%90/12345678",
+    uuid: "12345678",
+    ping_update_time: "2023-03-28T15:45:30.500Z",
+    id: "1",
+  },
+];
